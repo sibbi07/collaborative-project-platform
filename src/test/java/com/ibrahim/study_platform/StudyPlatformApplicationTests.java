018be1a05adc81ce09b416318a45ae4ced309854
Ibrahim Hassan <ibbihas3108@gmail.com>
2026-09-08 14:15:57 +0100

    Initial Spring Boot project setup

---
+++ src/test/java/com/ibrahim/study_platform/StudyPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibrahim.study_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
